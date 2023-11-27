247c5715b6125dba433c802887c1f997e34f625c
Dünya <dunya.gul.ynk@gmail.com>
2023-11-27 12:56:14 +0300

    Random joke completed

---
+++ Random_Joke_Generation/joke.js
@@ -0,0 +1,27 @@
+const jokebutton=document.getElementById('jokeButton');
+const joke=document.getElementById('joke');
+
+const apikey='Pz9sE8JtNyYvPpMrHSi49A==bLP7eDFZEIk2KrL3';
+const apiurl='https://api.api-ninjas.com/v1/dadjokes?limit=1'
+
+const options={
+    method:'GET',
+    headers:{
+        'X-Api-Key':apikey
+    }
+
+}
+ async function getJoke(){
+    joke.textContent='updating..';
+    jokebutton.textContent='Loading..';
+    jokebutton.disabled=true;
+
+   const response= await fetch(apiurl,options);
+   const data= await response.json();
+
+   jokebutton.disabled=false;
+   jokebutton.textContent='Tell me a joke';
+   joke.textContent=data[0].joke;
+ }
+
+ jokebutton.addEventListener('click',getJoke)
